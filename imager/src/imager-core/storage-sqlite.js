c9c8ea6f8ab86d2cd1cd118f1d28e47be8f0bb3f
Name <mail@local.local>
2017-09-15 16:06:33 +0200

    working

---
+++ imager/src/imager-core/storage-sqlite.js
@@ -0,0 +1,6 @@
+module.exports = (new ( function() {
+
+	
+
+
+}));
--- imager/src/imager-core/storage.js
+++ imager/src/imager-core/storage.js
@@ -1,7 +1,26 @@
 module.exports = (new ( function() {
+
+	var albums = {
+		map: {},
+		id: {}
+	};
 	
-	this.
+	this.getAlbumNameById = (name) => {
+
+	}
+
+	this.getAlbumIdByName = (id) => {
+
+	}
+
+	this.getAlbumById = (id) => {
+
+	}
+
+	this.getAlbumByName = (name) => {
+		var id = this.getAlbumIdByName(name);
+		return this.getAlbumById(id);
+	}
 
 
 }));
-
