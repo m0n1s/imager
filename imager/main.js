ee81d3c0537b96fbbe249271d1880e232a4e1499
Name <mail@local.local>
2017-09-20 16:15:12 +0200

    ui changes

--- imager/main.js
+++ imager/main.js
@@ -10,10 +10,12 @@ const url = require('url')
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
-
+electron.app.on('browser-window-created',function(e,window) {
+    window.setMenu(null);
+});
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 800, height: 600, title: false})
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
